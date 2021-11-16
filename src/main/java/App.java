3c26ffeb7e04cd46240bac321c82a31a4f7f710f
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-11-16 08:08:50 +0300

    add department creation and query routes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,37 @@
+import com.google.gson.Gson;
+import dao.Sql2oDepartmentDao;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUserDao;
+import models.Department;
+import org.sql2o.Sql2o;
+
 import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Sanity check");
-        get("/test","application/json",(request, response)->{
-            return "{\"Hello\":\"Bro\"}";
+        String connectionString = "jdbc:postgresql://localhost:5432/news_db";
+        Sql2o sql2o = new Sql2o(connectionString,"ftm","sparkpass");
+        Sql2oUserDao userDaoDao = new Sql2oUserDao(sql2o);
+        Sql2oNewsDao newsDao = new Sql2oNewsDao(sql2o);
+        Sql2oDepartmentDao departmentDao = new Sql2oDepartmentDao(sql2o);
+        Gson gson = new Gson();
+
+//        Api test
+//        System.out.println("Sanity check");
+//        get("/test","application/json",(request, response)->{
+//            return "{\"Hello\":\"Bro\"}";
+//        });
+
+        post("/departments/new", "application/json", (req, res) -> {
+            Department department = gson.fromJson(req.body(), Department.class);
+            departmentDao.addDepartment(department);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(department);
+        });
+
+        get("/departments", "application/json", (req, res) -> { //accept a request in format JSON from an app
+            res.type("application/json");
+            return gson.toJson(departmentDao.getAllDepartments());//send it back to be displayed
         });
     }
 }
