325d568d2465e3384fa1675a2d8482261cbab37a
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-11-16 08:03:18 +0300

    add news dao

---
+++ src/main/java/dao/Sql2oNewsDao.java
@@ -0,0 +1,85 @@
+package dao;
+
+import models.Department;
+import models.News;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oNewsDao implements NewsDao{
+    private final Sql2o sql2o;
+    public Sql2oNewsDao(Sql2o sql2o){
+        this.sql2o = sql2o;
+    }
+
+    public static void getDrivers(){
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void addNews(News news) {
+        getDrivers();
+        String sql = "INSERT INTO news (newsDescription, newsUrl, departmentId) VALUES (:newsDescription, :newsUrl, :departmentId)";
+        try(Connection conn = sql2o.open()){
+            int id = (int) conn.createQuery(sql,true)
+                    .bind(news)
+                    .executeUpdate()
+                    .getKey();
+            news.setId(id);
+        }catch(Sql2oException e){
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public List<News> getAllNews() {
+        getDrivers();
+        String sql = "SELECT * FROM news";
+        try(Connection conn = sql2o.open()){
+            return conn.createQuery(sql)
+                    .executeAndFetch(News.class);
+        }
+    }
+
+    @Override
+    public News getNewsById(int id) {
+        getDrivers();
+        try(Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM news WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(News.class);
+        }
+    }
+
+    @Override
+    public void deleteAllNews() {
+        getDrivers();
+        String sql = "DELETE FROM news";
+        try(Connection conn = sql2o.open()){
+            conn.createQuery(sql)
+                    .executeUpdate();
+        }catch(Sql2oException e){
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void deleteNewsById(int id) {
+        getDrivers();
+        String sql = "DELETE FROM news WHERE id = :id";
+        try(Connection conn = sql2o.open()){
+            conn.createQuery(sql)
+                    .addParameter("id",id)
+                    .executeUpdate();
+        }catch(Sql2oException e){
+            System.out.println(e);
+        }
+    }
+}
