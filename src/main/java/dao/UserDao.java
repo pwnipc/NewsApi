302d73ad02ce651f6245f4463ca4e1cfa8e46085
Chal13W1zz <charles.muthii@student.moringaschool.com>
2021-11-16 07:01:28 +0300

    update User Dao Interface

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -7,15 +7,15 @@ import java.util.List;
 public interface UserDao {
 
     //create
-    void addUser(User user){}
+    void addUser(User user);
 
     //read
-    List<User> getAllUsers(){}
-    User getUserById(int id){}
+    List<User> getAllUsers();
+    User getUserById(int id);
 
     //update
 
     //delete
-    void deleteUsers(){}
-    void deleteUserById(int id){}
+    void deleteUsers();
+    void deleteUserById(int id);
 }
